8cf000e70fae1240d9d1c5bc781fc6a82b1d6d60
Jo hanbin <whgksqls9999@naver.com>
2025-07-09 23:31:28 +0900

    chore: setting manifest and basic pwa

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: [],
+			// manifest: {
+			// 	name: 'Moa',
+			// 	short_name: 'Moa',
+			// 	start_url: '.',
+			// 	display: 'standalone',
+			// 	background_color: '#ffffff',
+			// 	theme_color: '#4A90E2',
+			// 	icons: [],
+			// },
+		}),
+	],
 });
