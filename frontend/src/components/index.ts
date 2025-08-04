7de1616428475496d4e134423d0474224d4c5fa8
Jo hanbin <whgksqls9999@naver.com>
2025-08-05 00:52:04 +0900

    feat: add loginForm widget

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,3 +1,4 @@
 export * from './layout';
 export * from './host';
 export * from './base';
+export * from './widget';
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,11 @@
-import { Input, PasswordInput } from '@components';
+import { Input, LoginForm, PasswordInput } from '@components';
 
 export function HomePage() {
 	return (
 		<div>
 			<PasswordInput />
 			<Input />
+			<LoginForm />
 		</div>
 	);
 }
