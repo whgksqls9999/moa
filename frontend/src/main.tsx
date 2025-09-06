ce7d5293c24bdc81a118b36303651c3d2af51764
Jo hanbin <whgksqls9999@naver.com>
2025-09-06 23:13:48 +0900

    feat: add some style

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './application/App.tsx';
-import { BrowserRouter } from 'react-router-dom';
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
